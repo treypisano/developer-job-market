a7f0554d9a670ae24d6f4a5033da2b7bce444e49
treypisano <treypisano@treypc>
2023-05-05 17:25:26 -0400

    add x and y to histogram

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import Example from "./scripts/example"
 import Histogram from "./scripts/histogram"
+import * as d3 from "d3";
+let testUrl = "http://api.adzuna.com/v1/api/jobs/gb/histogram?app_id=d536b8fb&app_key=6cecd9ddc0c642bdadfba824e14d21e3&location0=UK&location1=London&what=finance%20officer&content-type=application/json"
 
 document.addEventListener("DOMContentLoaded", () => {
+    // debugger
     let testHistogram = new Histogram()
     console.log("Hello world")
-    console.log(testHistogram.dataPoints)
+    console.log(testHistogram)
+
+        // .then (val => console.log(val))
+    // testHistogram.dataPoints.then(value => {
+    //     console.log(Object.keys(value.histogram))   
+    // })
 })
+
+// For now i will put the bar chart in here
--- src/scripts/histogram.js
+++ src/scripts/histogram.js
@@ -1,27 +1,35 @@
 // this is used to fetch and organize json data
 let testUrl = "http://api.adzuna.com/v1/api/jobs/gb/histogram?app_id=d536b8fb&app_key=6cecd9ddc0c642bdadfba824e14d21e3&location0=UK&location1=London&what=finance%20officer&content-type=application/json"
+import * as d3 from "d3";   
+// Look like the getch is returning the API object?
 
 class Histogram {
 
     constructor() {
-        this.dataPoints = getHistogramData()
+        
+        this.xValues = null
+        this.yValues = null
+        debugger
+        getHistogramData().then(val => {
+            this.xValues = Object.keys(val.histogram)
+            this.yValues = Object.values(val.histogram)
+        })
     }
 
+    static WIDTH = 500
+    static HEIGHT = 500
+    // static x_scale = d3.scaleBand().range([0, width])
+    // static y_scale = d3.scaleLinear().range([height, 0])
     // get dataPoints() {
     //     return this.dataPoints
-    // }
-
+    // 
     
 }
-
 async function getHistogramData() {
-    const response = await fetch(testUrl, {
-        method: "GET"
-        // mode: "no-cors"
-    })
-    
-    const jsonData = await response.json();
-    return jsonData;
+    // debugger
+    const res = await fetch(testUrl, {method: "GET"})
+    return res.json()
 }
+
 
 export default Histogram;
